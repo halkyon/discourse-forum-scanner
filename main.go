3f7cc72d6add35c5199c9335a9bdac023bbf6626
Sean Harvey <halkyon@gmail.com>
2022-04-11 08:43:10 +1200

    add more for release

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"net/url"
 	"os"
 	"os/signal"
+	"runtime"
+	"runtime/debug"
 	"time"
 
 	"github.com/halkyon/discourse-forum-scanner/internal/postchecker"
@@ -14,12 +16,17 @@ import (
 )
 
 const (
+	displayVersionFlag = "version"
 	discourseURLFlag   = "discourse-url"
 	checkIntervalFlag  = "check-interval"
 	filterKeywordsFlag = "filter-keywords"
 )
 
 var (
+	version        = "dev"
+	commit         = ""
+	date           = ""
+	displayVersion = flag.Bool(displayVersionFlag, false, "Display version information")
 	discourseURL   = flag.String(discourseURLFlag, "", "URL to Discourse instance")
 	checkInterval  = flag.Duration(checkIntervalFlag, 10*time.Minute, "Interval between getting posts")
 	filterKeywords = flag.String(filterKeywordsFlag, "", "Comma separated list of keywords to filter posts by")
@@ -39,7 +46,9 @@ func validateFlags() (err error) {
 	}
 
 	validateNotEmpty(discourseURLFlag, *discourseURL)
-	validateURL(discourseURLFlag, *discourseURL)
+	if *discourseURL != "" {
+		validateURL(discourseURLFlag, *discourseURL)
+	}
 
 	return err
 }
@@ -53,6 +62,23 @@ func main() {
 
 func run() error {
 	flag.Parse()
+
+	if *displayVersion {
+		result := version
+		if commit != "" {
+			result = fmt.Sprintf("%s\ncommit: %s", result, commit)
+		}
+		if date != "" {
+			result = fmt.Sprintf("%s\nbuilt at: %s", result, date)
+		}
+		result = fmt.Sprintf("%s\ngoos: %s\ngoarch: %s", result, runtime.GOOS, runtime.GOARCH)
+		if info, ok := debug.ReadBuildInfo(); ok && info.Main.Sum != "" {
+			result = fmt.Sprintf("%s\nmodule version: %s, checksum: %s", result, info.Main.Version, info.Main.Sum)
+		}
+		fmt.Println(result)
+		return nil
+	}
+
 	if err := validateFlags(); err != nil {
 		return err
 	}
